da593a7d84e074f0bd7bfdd3aaa39433d8f42faa
HoduGwaja <honeybutter9250@gmail.com>
2021-06-08 19:54:40 +0900

    [NodeJS-Rewrite | 2021.06.08 | first commit]

---
+++ 2/ES2015+/const_let.js
@@ -0,0 +1,17 @@
+if(true){
+    var x = 3; // 3
+}
+console.log(x);
+
+if(true){
+    const y = 3; // 에러
+}
+console.log(y);
+
+const a = 0;
+a = 1; // 에러
+
+let b = 0;
+b = 1; // 1
+
+const c; // error(값이 없음)
